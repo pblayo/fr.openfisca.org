a49865ef890f2a036c23c0be191ae9f387627fc8
Anna-Livia <annalivia.gomart@gmail.com>
2018-02-05 19:17:17 +0100

    New tutos

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Ceci est le code source du [site web](http://fr.openfisca.org) de la communauté
 ## [EN] Introduction
 [OpenFisca](http://openfisca.org/en) is a versatile microsimulation free software.  
 This is the source code of the [web site](http://fr.openfisca.org) for the community working on the french tax and benefit system.
+
+
+## Installation
+- node 9 (via nvm)
+- yarn (`brew install yarn --without-node`)
+- yarjn init
+- yarn add next
+- yarn add react
+- yarn add react-dom
+- add script 
+- create pages directory
+- create index.js in pages
--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,11 @@
-import React from 'react'
-import posts from '../data/post'
-import Link from 'next/link'
+import Layout from '../comps/MyLayout'
 
-export default class extends React.Component {
-  static getInitialProps () {
-    return { posts: posts }
-  }
+const Index = () => (
+  <div>
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  </div>
+)
 
-  render () {
-    return (
-      <div>
-      <div>
-        <h3> NEXTHRONE - THE REVELATION OF GAME OF THRONES CHARACTERS </h3>
-      </div>
-      <table className="this_table">
-        <thead id="this_header">
-          <tr>
-              <th id="this_th">Character</th>
-              <th>Real Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-              this.props.posts.map( (post, i) => (
-                  <tr key={i}>
-                      <td>{ post.codeName }</td>
-                      <td>
-                        <Link href={`/account?id=${post.id}`}>{ post.realName }</Link>
-                      </td>
-                  </tr>
-              ))
-          }
-       </tbody>
-       <style jsx>{`
-      th {
-	    background: #00cccc;
-	    color: #fff;
-	    text-transform: uppercase;
-	    fontSize: 12px;
-	    padding: 12px 35px;
-	  }
-	  header {
-	    font: 15px Monaco;
-	    text-align: center;
-	  }
-	  table {
-	    font-family: Arial;
-	    margin: 25px auto;
-	    border-collapse: collapse;
-	    border: 1px solid #eee;
-	    border-bottom: 2px solid #00cccc;
-	  }
-
-	  td {
-	    color: #999;
-	    border: 1px solid #eee;
-	    padding: 12px 35px;
-	    border-collapse: collapse;
-	  }
-
-	  list {
-	    padding: 50px;
-	    text-align: center;
-	  }
-
-	  photo {
-	    display: inline-block;
-	  }
-
-	  photoLink {
-	    color:	 #333;
-	    verticalAlign: middle;
-	    cursor:	 pointer;
-	    background:	#eee;
-	    display: inline-block;
-	    width: 250px;
-	    height: 250px;
-	    line-height:	250px;
-	    margin:	10px;
-	    border:	2px solid transparent;
-
-	    }
-    `}</style>
-      </table>
-      </div>
-    )
-  }
-}
+export default Index
